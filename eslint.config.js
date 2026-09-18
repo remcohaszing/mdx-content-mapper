63a7412d1da2aeddfb17f3cdc8c9c130ddb61429
Remco Haszing <remcohaszing@gmail.com>
2026-09-18 12:54:33 +0200

    Update lockfile

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ export default define([
   { ignores: ['fixtures', 'lib/protocol.ts'] },
   {
     rules: {
-      'no-inline-comments': 'off',
       'no-param-reassign': 'off',
       'import-x/no-relative-packages': 'off',
       'jsdoc/reject-any-type': 'off',
@@ -12,8 +11,7 @@ export default define([
       'jsdoc/require-param-description': 'off',
       'jsdoc/require-returns-description': 'off',
       'jsdoc/require-template-description': 'off',
-      'unicorn/consistent-destructuring': 'off',
-      'unicorn/prefer-code-point': 'off'
+      'unicorn/consistent-destructuring': 'off'
     }
   }
 ])
